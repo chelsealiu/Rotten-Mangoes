cf2f5f0bdde89398e2ef8f2debd074c14a074e37
Chelsea Liu <cl314@duke.edu>
2015-07-02 15:19:18 -0400

    Added MapView to movie reviews app.

---
+++ Rotten Mangoes/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  Rotten Mangoes
+//
+//  Created by Chelsea Liu on 7/2/15.
+//  Copyright (c) 2015 Chelsea Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
